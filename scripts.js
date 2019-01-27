9f40b14e130fab511453a2db38bc2b2ef5ef73c8
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-01-27 20:00:38 +0100

    Add js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,12 @@
-var mapWidth = 400;
-var mapHeight = 400;
-var counter = 0;
+var mapWidth = 600;
+var mapHeight = 600;
+var counterClicks = 0;
+var limitClicks = 15;
 
 //jako argument później przekażemy mapWidth i mapHeight
 function randomNumber(size) {
   return Math.floor(Math.random() * size)
 }
-
-//losujemy  miejsce ukrycia skarbu
-var treasure = {
-  x: randomNumber(mapWidth),
-  y: randomNumber(mapHeight)
-}
-
-/
-
 
 //Obliczanie odległości między kliknięciem a celem
 //offsetX - szerokość na jakiej nastąpiło zdarzenie
@@ -28,7 +20,7 @@ function calculateDistance(event, treasure) {
   return Math.sqrt((distanceX * distanceX) + (distanceY * distanceY))
 }
 
-/tworzymy podpowiedzi
+//tworzymy podpowiedzi
 function createPrompts(distance) {
   if (distance < 10) {
     return "Parzy!";
@@ -47,7 +39,29 @@ function createPrompts(distance) {
   }
 }
 
-$('.map').click(function(e) {
-  counter++;
+//losujemy  miejsce ukrycia skarbu
+var treasure = {
+  x: randomNumber(mapWidth),
+  y: randomNumber(mapHeight)
+}
 
+
+$('.map').click(function(e) {
+    counterClicks++;
+    
+      if (counterClicks > limitClicks) {
+        alert('GAME OVER');  
+        return
+    }
+    
+    var distance = calculateDistance(event, treasure);
+    var prompt = createPrompts(distance);
+    var click = limitClicks - counterClicks;
+    $('.distance').text(prompt);
+    $('.clicks').text(`Pozostało Ci ${limitClicks - counterClicks} kliknięć!`)
+    
+    if (distance < 8) {
+        alert('Skarb został znaleziony po' + counterClicks + 'kliknięciach')
+  }
 })
+
