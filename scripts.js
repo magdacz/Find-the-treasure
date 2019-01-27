b936538486cfce08f42da693222afc7b7a9b9ca1
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-01-27 14:00:47 +0100

    Add code in js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,17 @@ var mapWidth = 400;
 var mapHeight = 400;
 var counter = 0;
 
+//jako argument później przekażemy mapWidth i mapHeight
 function randomNumber(size) {
   return Math.floor(Math.random() * size)
 }
 
+var treasure = {
+  x: randomNumber(mapWidth),
+  y: randomNumber(mapHeight)
+}
+
+$('.map').click(function(e) {
+  counter++;
+
+})
